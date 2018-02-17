8a681f8cc6e24640873e09c600835f6b49171a30
John Stein <john@johnandkerri.com>
2018-02-17 10:47:05 -0500

    deploy

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,63 +1,22 @@
 #!/usr/bin/env bash
 
+echo 'deploy.sh'
 
-echo 'DEPLOYING'
+# Make sure we are in the right directory.
+MY_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $MY_DIR
+cd ..
 
+# Sync with S3
+aws s3 sync \
+  ./ \
+  s3://thekellyschool/toocool \
+  --exclude=.git/** \
+  --exclude=bin/** \
+  --exclude=README.md
 
+# Tell CloudFront that there are new files on S3 to fetch.
+aws cloudfront create-invalidation \
+  --distribution-id E1UVEVWIZ244PD \
+  --paths '/*'
 
-# #!/usr/bin/env bash
-# set -e
-# MY_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-# GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
-# cd $MY_DIR
-# cd ../node/deploy
-# rm -rf node_modules
-# . ~/.nvm/nvm.sh > /dev/null 2>&1
-# npm install > /dev/null 2>&1
-# nvm use 6 > /dev/null 2>&1
-# SYNC_DIR="$(./deploy.js)"
-# rm -rf node_modules
-# aws s3 sync --exclude 'jscss/*' --delete --content-type text/html $SYNC_DIR s3://glo.sh/$GIT_BRANCH
-# aws s3 sync --include 'jscss/*' $SYNC_DIR s3://glo.sh/$GIT_BRANCH
-# aws cloudfront create-invalidation \
-#   --distribution-id E29S1AZ8HEMEO5 \
-#   --paths '/*'
-
-
-# vi ~/sync-website-to-s3.sh
-
-# #!/usr/bin/env bash
-
-# cd /home/ubuntu/static-web
-# OUTPUT="$(git pull)"
-# aws s3 sync /home/ubuntu/static-web s3://thekellyschool/website \
-#   --exclude=.git/** \
-#   --exclude=README.md
-# if [ "$OUTPUT" != "Already up-to-date." ]; then
-#   aws cloudfront create-invalidation \
-#   --distribution-id E2HT5RYWP92IC4 \
-#   --paths '/*'
-# fi
-
-# chmod +x ~/sync-website-to-s3.sh
-
-# crontab -e
-# * * * * * /home/ubuntu/sync-website-to-s3.sh >/dev/null 2>&1
-
-
-# set -e
-# MY_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-# GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
-# cd $MY_DIR
-# cd ../node/deploy
-# rm -rf node_modules
-# . ~/.nvm/nvm.sh > /dev/null 2>&1
-# npm install > /dev/null 2>&1
-# nvm use 6 > /dev/null 2>&1
-# SYNC_DIR="$(./deploy.js)"
-# rm -rf node_modules
-# aws s3 sync --exclude 'jscss/*' --delete --content-type text/html $SYNC_DIR s3://glo.sh/$GIT_BRANCH
-# aws s3 sync --include 'jscss/*' $SYNC_DIR s3://glo.sh/$GIT_BRANCH
-# aws cloudfront create-invalidation \
-#   --distribution-id E29S1AZ8HEMEO5 \
-#   --paths '/*'
--- bin/pullme.sh
+++ bin/pullme.sh
@@ -1,20 +1,25 @@
 #!/usr/bin/env bash
 set -e
-#
-# Check and see if I have been updated.
-# If not, do nothing. If yes, run my deployment script.
-#
-# We can run this every minute.
-# crontab -e
-# * * * * * /home/ubuntu/glo.sh/bin/pullme.sh >/dev/null 2>&1
 
+# We can run this every minute. It's cheap and safe.
+# crontab -e
+# * * * * * pullme.sh >/dev/null 2>&1
+
+# Make sure we are in the right directory.
 MY_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $MY_DIR
 cd ..
+
+# Pull down changes from git.
 OUTPUT="$(git pull)"
+echo "$OUTPUT"
 
-echo "OUTPUT: $OUTPUT"
-
-if [[ "$OUTPUT" != *"Already up-to-date"* ]]; then
+# Check and see if I have been updated.
+if [[ "$OUTPUT" = "Already"* ]]; then
+  # If not, do nothing.
+  echo "Not Deploying"
+else
+  # If yes, run my deployment script.
+  echo "Deploying"
   $MY_DIR/deploy.sh
 fi
